e63bed332fcb1aaadfb7ee58f74d89fdece616b2
Shahryar <shahryarzafar2525@gmail.com>
2026-06-03 14:21:35 +0500

    open ai added

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:5173', 
-  'https://careercompassfront-3981.vercel.app',
-  "'https://careercompassfront-lc3s.vercel.app'",
+  "https://careercompassfront-3981.vercel.app",
   "https://careercompassfront.vercel.app"
 ];
 
